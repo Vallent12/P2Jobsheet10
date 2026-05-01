1882eab08e4d744e4b52f3a5876d58bb60bd9582
Vallent12 <fevadtya09.gmail.com>
2026-05-01 15:14:18 +0700

    Percobaan 2

---
+++ Mahasiswa03.java
@@ -0,0 +1,14 @@
+public class Mahasiswa03 {
+    String nim, nama, prodi, kelas;
+
+    public Mahasiswa03(String nim, String nama, String prodi, String kelas) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi;
+        this.kelas = kelas;
+    }
+
+    public void tampilkanData() {
+        System.out.println(nim + " - " + nama + " - " + prodi + " - " + kelas);
+    }
+}
